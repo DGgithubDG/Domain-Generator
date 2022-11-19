f91c974cb60ad11f0d35a1966be8c97d7ea459f6
DGgithubDG <111754374+DGgithubDG@users.noreply.github.com>
2022-11-19 19:33:35 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronouns = ["the", "our", "their", "his"];
+let adjs = ["great", "big", "small", "huge", "giant"];
+let nouns = ["jogger", "racoon", "rat", "robber", "thief"];
+let domain = [".com", ".us", ".eu"];
+let emptyArray = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronouns.length; i++) {
+  for (let e = 0; e < adjs.length; e++) {
+    for (let o = 0; o < nouns.length; o++) {
+      for (let u = 0; u < domain.length; u++) {
+        emptyArray.push(
+          "" + pronouns[i] + adjs[e] + nouns[o] + domain[u] + "<br/>"
+        );
+      }
+    }
+  }
+}
+
+console.log(emptyArray);
+
+document.querySelector("#domain-results").innerHTML = emptyArray;
+//console.log(pronoun[0].concat("hello", ".com "));
+//console.log(adj[0].concat("goodbye", ".us "));
+////console.log(noun[0].concat("how", ".org "));
+//console.log(pronoun[1].concat("when", ".oi "));
+////console.log(adj[1].concat("tonight", ".co.uk "));
+//console.log(noun[1].concat("yesterday", ".us "));
